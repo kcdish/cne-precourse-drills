b5c4ac6fc2937db468824692100546b3a773af76
Unknown <kevin.clark@dish.com>
2017-03-13 12:54:52 -0600

    Implemented address, addressable and business classes

---
+++ src/main/java/com/galvanize/Business.java
@@ -0,0 +1,22 @@
+package com.galvanize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Business implements Addressable {
+
+	private final String name;
+	private final List<Address> addresses = new ArrayList<Address>();
+	
+	public Business (String name){
+		this.name = name;
+	}
+	
+	
+	public String getName () { return name; }
+	@Override
+	public List<Address> getAddresses() { return addresses; }
+	@Override 
+	public void addAddress(Address new_address) { addresses.add(new_address);}
+
+}
